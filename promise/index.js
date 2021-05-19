dc9b2e384ec7f5e75de1d090d3b66f94029cafeb
Fish <icyyfish@gmail.com>
2021-05-20 07:43:11 +0800

    🐳 chore: add ignore file & some comment

--- promise/index.js
+++ promise/index.js
@@ -1,3 +1,6 @@
+// 100 行代码实现 Promises/A+ 规范
+// https://mp.weixin.qq.com/s?__biz=MzA4Njc2MTE3Ng==&mid=2456151398&idx=1&sn=c3351d6c9eb166035f2fa97a1b0b3a0a&chksm=88528ed1bf2507c797c05a4423bbc53e6e450c05d3da6bc6ad1825487712c156903891890c7c&token=1869489865&lang=zh_CN&scene=21#wechat_redirect
+
 const isFunction = obj => typeof obj === "function";
 const isObject = obj => typeof obj === "object" && obj !== null;
 const isThenable = obj => (isFunction(obj) || isObject(obj)) && "then" in obj;
